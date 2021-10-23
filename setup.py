297746810da9e178098a50b7a349f759f855af9a
Daniel Pope <mauve@mauveweb.co.uk>
2021-10-23 14:01:47 +0100

    Create package that tells you to upgrade pip.

---
+++ setup.py
@@ -0,0 +1,9 @@
+import sys
+sys.exit("""\
+You have downloaded a manylinux1 wheel (.whl), probably due to using
+an outdated version of pip.
+
+manylinux1 is not supported by this project.
+
+Please upgrade to the latest version of pip.
+""")
