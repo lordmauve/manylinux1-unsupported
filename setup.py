eeec480f241bd283ff2a6ddce483502f3d37c0bd
Daniel Pope <mauve@mauveweb.co.uk>
2021-10-23 14:18:11 +0100

    Add more detail on the strategy for this package

--- setup.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.exit("""\
-You have downloaded a manylinux1 wheel (.whl), probably due to using
-an outdated version of pip.
-
-manylinux1 is not supported by this project.
-
-Please upgrade to the latest version of pip.
-""")
